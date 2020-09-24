d0b1e6cb85d4e09fca0563bdfd87242500eb1718
manzanarez83 <cmanzanarez07@gmail.com>
2020-09-24 13:16:05 -0600

    repo setup

---
+++ rapido.sh
@@ -0,0 +1,7 @@
+echo "# no_namev2" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M master
+git remote add origin git@github.com:manzanarez83/no_namev2.git
+git push -u origin master
