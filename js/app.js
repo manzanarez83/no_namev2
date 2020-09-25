821aa481d13ff9102645a6513b659363836be64c
manzanarez83 <cmanzanarez07@gmail.com>
2020-09-25 11:38:46 -0600

    server tes

--- js/app.js
+++ js/app.js
@@ -1,4 +1,13 @@
-var numero1 = 2
-var numero2 =3
+//var numero1 = 2
+//var numero2 =3
 
-console.log(numero1+numero2);
+//console.log(numero1+numero2);
+
+const http = require('http')
+
+const server = http.createServer((req, res) => {  
+    res.end("hello world from node js");
+
+});
+
+server.listen(3000) ;
