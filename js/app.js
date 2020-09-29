34cfdcd789bd6a0d3b9ddb5e225b3a74054a92b8
manzanarez83 <cmanzanarez07@gmail.com>
2020-09-29 12:01:54 -0600

    express installing

--- js/app.js
+++ js/app.js
@@ -11,3 +11,17 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(3000) ;
+
+const express=require('express')
+const app=express()
+
+
+app.get("/", req, res) => {
+    res.send("Hello world from node js");
+    const port = 8080
+    app.listen(port,() => {console.log('A node Js API is listening on port: ${port}');
+});
+
+    
+
+});
